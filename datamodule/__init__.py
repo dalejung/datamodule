1d0cfe6f4c4f44782d1912d73a103fcb9447042a
Dale Jung <dale@dalejung.com>
2013-06-16 21:22:02 -0400

    removed the _CACHE_MANAGER singleton, sys.meta_path already implicitly does this

--- datamodule/__init__.py
+++ datamodule/__init__.py
@@ -4,9 +4,11 @@ import sys
 
 from loader import DataModuleLoader
 from finder import DataModuleFinder
-from cache import CacheManager
+from cache import CacheManager, DMCache
 
-_CACHE_MANAGER = CacheManager()
-
-def install_datamodule(loader_class=DataModuleLoader, cache_manager=_CACHE_MANAGER):
+def install_datamodule(loader_class=DataModuleLoader, cache_manager=None, cache_cls=DMCache, override=False):
+    if cache_manager is None:
+        cache_manager = CacheManager(cache_cls)
+    if sys.meta_path and not override:
+        raise Exception('sys.meta_path is not empty')   
     sys.meta_path = [DataModuleFinder(loader_class, cache_manager=cache_manager)]
